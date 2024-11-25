a9c4d1dd3902aa20277f417ac280ad9ce0710f14
OssZz <oscarsalazar1119368@correo.itm.edu.co>
2024-11-25 17:44:15 -0500

    se realizan actualizaciones

--- src/main/java/com/java/projecto/Supervisor.java
+++ src/main/java/com/java/projecto/Supervisor.java
@@ -5,5 +5,26 @@ import com.java.projecto.modelos.SupervisorModelo;
 
 public class Supervisor {
 
-    private SupervisorModelo[] listaDiseno = new SupervisorModelo[50];
+    private SupervisorModelo[] listaSupervisores = new SupervisorModelo[50];
+    
+    
+    public void setListaDiseno(SupervisorModelo[] listaDiseno) {
+        this.listaSupervisores = listaDiseno;
+    }
+
+    public void setAgregarProyecto(int identificacion){
+        int numero_proyecto;
+        
+        for (SupervisorModelo listaSupervisor : listaSupervisores) {
+            if (listaSupervisor != null) {
+                if (listaSupervisor.getIdentificacion() == identificacion) {
+                    numero_proyecto = listaSupervisor.getProyecto_supervisado() +1;
+                    listaSupervisor.setProyecto_supervisado(numero_proyecto);
+                }
+            }
+
+        }
+    }
+
 }
+
